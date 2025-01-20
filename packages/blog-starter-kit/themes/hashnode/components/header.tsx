eafc96bd1bd6ed915587dc6a8717a06bcc06ed28
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2025-01-20 19:18:36 +0000

    Reapply "Adding offer marque"
    This reverts commit 0ff15d499155f58ce270babc0d430c347b43f62b.

--- packages/blog-starter-kit/themes/hashnode/components/header.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/header.tsx
@@ -33,6 +33,12 @@ export const Header = (props: Props) => {
 		<header
 			className="blog-header relative z-50 w-full border-b border-black/10 bg-white bg-opacity-70 dark:border-white/10 dark:bg-slate-900 dark:bg-opacity-70"
 		>
+			<div className="bg-brand-orange text-white text-center py-2 overflow-hidden">
+    			<div className="marquee">
+      				<span>🎉 Limited Time Offer: Get <b>350rs off</b> on all course till 26th Jan! Use code: <b>REPUBLICDAY</b> at checkout 🎉</span>
+   	 			</div>
+			</div>
+
 			<div className="container mx-auto px-2 md:px-4 2xl:px-10">
 				<div className="relative z-40 flex flex-row items-center justify-between pb-2 pt-8 md:mb-4">
 					<div className="flex flex-row items-center py-1">
