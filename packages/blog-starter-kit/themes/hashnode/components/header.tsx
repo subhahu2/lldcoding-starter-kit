2b35d0ea7b7aa3fc5110349e15368fe014978f3d
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2025-02-11 17:27:09 +0000

    Remove offer

--- packages/blog-starter-kit/themes/hashnode/components/header.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/header.tsx
@@ -33,11 +33,11 @@ export const Header = (props: Props) => {
 		<header
 			className="blog-header relative z-50 w-full border-b border-black/10 bg-white bg-opacity-70 dark:border-white/10 dark:bg-slate-900 dark:bg-opacity-70"
 		>
-			<div className="bg-brand-orange text-white text-center py-2 overflow-hidden">
+			{/* <div className="bg-brand-orange text-white text-center py-2 overflow-hidden">
     			<div className="marquee">
       				<span>🎉 Limited Time Offer: Get <b>150rs off</b> on all course till 10th FEB! Use code: <b>FLASH150</b> at checkout 🎉</span>
    	 			</div>
-			</div>
+			</div> */}
 
 			<div className="container mx-auto px-2 md:px-4 2xl:px-10">
 				<div className="relative z-40 flex flex-row items-center justify-between pb-2 pt-8 md:mb-4">
