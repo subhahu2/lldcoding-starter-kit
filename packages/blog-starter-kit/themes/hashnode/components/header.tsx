098897956fa3fa23d784f6c1e07bd32667f0e265
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2025-08-29 13:33:05 +0530

    Update header.tsx

--- packages/blog-starter-kit/themes/hashnode/components/header.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/header.tsx
@@ -36,11 +36,11 @@ export const Header = (props: Props) => {
 		<header
 			className="blog-header relative z-50 w-full border-b border-black/10 bg-white bg-opacity-70 dark:border-white/10 dark:bg-slate-900 dark:bg-opacity-70"
 		>
-			{/* <div className="bg-brand-orange text-white text-center py-2 overflow-hidden">
+			<div className="bg-brand-orange text-white text-center py-2 overflow-hidden">
     			<div className="marquee">
-      				<span>BIG DROP | 🎉 Limited Time Offer: Get <b>400rs off</b> on our lld course for today! Use code: <b> BIGDROP </b> at checkout 🎉</span>
+      				<span>BIG DROP | 🎉 Limited Time Offer: Get <b>400rs off</b> on lifetime lld course for today! Use code: <b> 400AUG2025 </b> at checkout 🎉</span>
    	 			</div>
-			</div> */}
+			</div>
 			<SubscriptionDaysCounter />
 
 			<div className="container mx-auto px-2 md:px-4 2xl:px-10">
