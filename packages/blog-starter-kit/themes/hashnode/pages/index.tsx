90dd91e34ba49426361b3469b55f8bc6b0b6a08d
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2025-10-05 19:14:26 +0000

    Adding Reddit script

--- packages/blog-starter-kit/themes/hashnode/pages/index.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/index.tsx
@@ -127,6 +127,18 @@ export default function Index(
 							__html: JSON.stringify(addPublicationJsonLd(publication)),
 						}}
 					/>
+
+					{/* Reddit Pixel */}
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_h73veni5j70y');rdt('track', 'PageVisit');
+							`,
+						}}
+					/>
+					{/* DO NOT MODIFY UNLESS TO REPLACE A USER IDENTIFIER */}
+					{/* End Reddit Pixel */}
+
 				</Head>
 				<Header isHome={true} />
 				<div>
