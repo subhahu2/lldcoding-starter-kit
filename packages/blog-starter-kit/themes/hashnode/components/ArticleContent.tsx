e0d68397c9ac986467401a6b234a4e53da9363de
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2025-11-23 17:23:29 +0000

    use 2 instead of 5

--- packages/blog-starter-kit/themes/hashnode/components/ArticleContent.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/ArticleContent.tsx
@@ -92,7 +92,7 @@ const ArticleContent: React.FC<ArticleContentProps> = React.memo(({ memoizedPost
   }, [hasMounted]);
 
   useEffect(() => {
-    if (articleViews > 5 && !showLimitModal) setShowLimitModal(true);
+    if (articleViews > 2 && !showLimitModal) setShowLimitModal(true);
   }, [articleViews]);
 
   const handleSignIn = useCallback(() => setHasSignedIn(true), []);
@@ -125,7 +125,7 @@ const ArticleContent: React.FC<ArticleContentProps> = React.memo(({ memoizedPost
     return postContent;
   }
 
-  if (articleViews > 5) {
+  if (articleViews > 2) {
     if(showLimitModal) {
       return <ArticleLimitModal onClose={handleCloseLimitModal} />;
     }
