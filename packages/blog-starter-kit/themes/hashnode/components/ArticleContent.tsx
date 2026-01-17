b1a5e3ad5e52724b2d150f1511f6b7000ce95988
subhahu2 <158217829+subhahu2@users.noreply.github.com>
2026-01-17 18:28:05 +0000

    increase free limit to 10x

--- packages/blog-starter-kit/themes/hashnode/components/ArticleContent.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/ArticleContent.tsx
@@ -92,7 +92,7 @@ const ArticleContent: React.FC<ArticleContentProps> = React.memo(({ memoizedPost
   }, [hasMounted]);
 
   useEffect(() => {
-    if (articleViews > 4 && !showLimitModal) setShowLimitModal(true);
+    if (articleViews > 40 && !showLimitModal) setShowLimitModal(true);
   }, [articleViews]);
 
   const handleSignIn = useCallback(() => setHasSignedIn(true), []);
@@ -194,7 +194,7 @@ const ArticleContent: React.FC<ArticleContentProps> = React.memo(({ memoizedPost
     return postContent;
   }
 
-  if (articleViews > 4) {
+  if (articleViews > 40) {
     if(showLimitModal) {
       return <ArticleLimitModal onClose={handleCloseLimitModal} />;
     }
